83745d7ced6152bfe177ab79b3f5593fccb3e68b
Emily-Campbell23 <emcampbel@hartford.edu>
2024-03-28 09:11:29 -0400

    added options

--- Circus.java
+++ Circus.java
@@ -8,6 +8,12 @@ public class Circus{
    System.out.println("You take a look around the new room. In the left hand corner there is" +
    " a large group of clowns do circus tricks in the corner, juggling, throwing knives " +
    "and playing on a unicycle. In the middle acrobats wearing all \ndifferent colors doing their sky" +
-   "high tricks. In the lower right hand corner a large cage stands empty with \nstraw on the floor...");
+   "high tricks. In the lower right hand corner a large cage stands empty with \nstraw on the floor..."+
+   " there is only one door on the other side of the room. It seems to require a large key and" +
+   "requires three" + "colors to be put in.");
+   System.out.println("\n What will you do next?");
+   System.out.println("\n1) Go to the group of clowns" +
+   "\n2) Go watch the acrobats" + "\n3) Go to the door" + "\n4) Go to the cage");
+   
     }
 }
