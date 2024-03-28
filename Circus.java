76e9f4ab3a6ed8f9f79cf9d360c60fea301e3e38
Emily-Campbell23 <emcampbel@hartford.edu>
2024-03-28 09:42:37 -0400

    finished clowns

--- Circus.java
+++ Circus.java
@@ -6,8 +6,7 @@ public class Circus{
    System.out.println("Congratulations! You have survived the Vampire room. You now have 100 points.");
    System.out.println("You breath a sigh of relief... you are safe for now");
    System.out.println("You take a look around the new room. In the left hand corner there is" +
-   " a large group of clowns do circus tricks in the corner, juggling, throwing knives " +
-   "and playing on a unicycle. In the middle acrobats wearing all \ndifferent colors doing their sky" +
+   " a large group of clowns do circus tricks in the corner. In the middle acrobats wearing all \ndifferent colors doing their sky" +
    "high tricks. In the lower right hand corner a large cage stands empty with \nstraw on the floor..."+
    " there is only one door on the other side of the room.");
    System.out.println("\n What will you do next?");
@@ -18,7 +17,7 @@ public class Circus{
    String choiceString = "";
 
    switch(choice){
-    case 1: choiceString = "You walk over to the group of clows. They just seem to be doing party tricks.";
+    case 1: choiceString = "You walk over to the group of clowns. They just seem to be doing party tricks.";
     break;
     case 2: choiceString = "You watch the acrobats and notice they are wearing different colors, blue, red, and green";
     break;
@@ -26,6 +25,22 @@ public class Circus{
     break;
     case 4: choiceString = "You walk over to the cage. There are large scratch marks on the floor and you notice a key covered by hay.";
     break;
+   }
+   System.out.println(choiceString);
+   if(choice == 1){
+   System.out.println("What will you do now?");
+   System.out.println("1) investigate the clowns" + "\n2) exit ");
+
+   int choice1 = scan.nextInt();
+   String choice1String = "";
+
+   switch(choice1){
+    case 1: choice1String = "You notice the clowns are doing three different party tricks including juggling, throwing knives, and playing \non a unicycle";
+    break;
+    case 2: choice1String = "You back away from the clowns...";
+    break;
+   }
+   System.out.println(choice1String);
    }
    
     }
