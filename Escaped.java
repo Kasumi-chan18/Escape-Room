b183e417f94051f417191c9186faf3aca7aa2a9f
Emily-Campbell23 <emcampbel@hartford.edu>
2024-04-16 09:26:55 -0400

    added in some ending

--- Escaped.java
+++ Escaped.java
@@ -1,3 +1,17 @@
-public class Escaped {
-  
+public class Escaped{
+    Room zombiesRoom = new Zombies();
+
+    zombiesRoom.getScore() = score;
+
+    System.out.println("You have survived the wrath of the white witch.");
+    System.out.println("Congratulations! You successfully escaped the haunted mansion...");
+    System.out.println("You have ended with" + score + 100);
+    System.out.println("You have XXXX number of lives left");
+
 }
+
+
+// add in an array
+// fix all current errors
+// add in inheritence
+// add in polymorphism
