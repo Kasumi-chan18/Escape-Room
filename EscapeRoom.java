7b50d85d50eb6e0a3325ca21fabd9c15a445d0bb
MacKenzieColeman <macoleman@hartford.edu>
2024-04-29 22:33:55 -0400

    i forgot to add instructions for the inventory

--- EscapeRoom.java
+++ EscapeRoom.java
@@ -6,7 +6,7 @@ public class EscapeRoom {
     public static void main(String[] args) {
         Player player = new Player();
         System.out.println("Welcome to the Escape Room game!");
-        System.out.println("Instructions:\n1. Explore different rooms using commands like 'Go to', 'Investigate', 'Open', 'Pick up', and 'Exit'.");
+        System.out.println("Instructions:\n1. Explore different rooms using commands like 'Go to', 'Investigate', 'Open', 'Pick up', 'Exit', and 'Inventory'.");
         System.out.println("2. Earn points by achieving goals in each room.");
         System.out.println("3. One wrong move and you may just lose a life...");
         System.out.println("4. Try to escape the mansion by reaching the final room.");
